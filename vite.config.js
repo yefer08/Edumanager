6e547d24b2db1aac575c815322dd95db375d649b
yefer <yexcordoba@outlook.com>
2025-11-06 01:50:16 -0500

    Add @rollup/plugin-commonjs to fix Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from '@rollup/plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,12 @@ export default defineConfig({
   build: {
     target: 'esnext',
     rollupOptions: {
+      plugins: [
+        commonjs({
+          include: /node_modules/,
+          transformMixedEsModules: true
+        })
+      ],
       output: {
         manualChunks: (id) => {
           if (id.includes('firebase') || id.includes('@firebase')) {
@@ -14,10 +21,6 @@ export default defineConfig({
           }
         }
       }
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
     }
   },
   optimizeDeps: {
